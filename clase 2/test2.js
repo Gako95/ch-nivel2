958ed5165308101e12a41c7fd36f0d8a1ef35ceb
Gako <gako95@gmail.com>
2016-11-30 21:28:40 -0300

    nivel 2 comenzado

---
+++ clase 2/test2.js
@@ -0,0 +1,28 @@
+var semana = ["lunes","martes","miercoles","jueves","viernes","sabado","domingo"];	
+var dia=prompt("ingrese dia");
+if(dia==semana[5]){
+	alert ("saturday");
+}
+if (dia==semana[6]){
+	alert("sunday");
+}
+		
+var edad=prompt("ingrese edad");
+if((edad>=18) && (dia==semana[5])){
+	alert("partie");
+}
+if(edad<=10){
+	alert("kid");
+}
+if((edad>=20)&&(edad<30)){
+	alert("twenties");
+}
+
+for(var i=0;i<semana.length;i++){
+	if(semana[i]==="lunes"){
+		alert("garfield");
+	}
+	if((semana[i]==="viernes")&&(edad>=18)){
+		alert("more partie")
+	}
+}
