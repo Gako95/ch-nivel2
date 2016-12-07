a2fcc69ade373a47fa2552094bded6c2d544ce15
Gako <gako95@gmail.com>
2016-12-07 18:47:14 -0300

    agrego clase 3

--- clase 2/test2.js
+++ clase 2/test2.js
@@ -3,7 +3,7 @@ var dia=prompt("ingrese dia");
 if(dia==semana[5]){
 	alert ("saturday");
 }
-if (dia==semana[6]){
+else if (dia==semana[6]){
 	alert("sunday");
 }
 		
@@ -11,10 +11,10 @@ var edad=prompt("ingrese edad");
 if((edad>=18) && (dia==semana[5])){
 	alert("partie");
 }
-if(edad<=10){
+else if(edad<=10){
 	alert("kid");
 }
-if((edad>=20)&&(edad<30)){
+else if((edad>=20)&&(edad<30)){
 	alert("twenties");
 }
 
@@ -22,7 +22,7 @@ for(var i=0;i<semana.length;i++){
 	if(semana[i]==="lunes"){
 		alert("garfield");
 	}
-	if((semana[i]==="viernes")&&(edad>=18)){
+	else if((semana[i]==="viernes")&&(edad>=18)){
 		alert("more partie")
 	}
 }
